a8681fe0dcd160ab5da6c91350fdc248848beac1
Ayush Raj <ayushcode1@gmail.com>
2023-03-03 15:46:22 +0530

    rupeeCOIN Blockchain implementation part 1

---
+++ Blockchain Implementation/rupeeCoin.js
@@ -0,0 +1,103 @@
+const SHA256 = require("crypto-js/sha256");
+class CryptoBlock {
+  constructor(height, data, previousHash = " ") {
+    this.height = height;
+    this.timestamp = Date.now();
+    this.data = data;
+    this.previousHash = previousHash;
+    this.currentHash = this.computeHash();
+    this.nonce = 0;
+  }
+
+  computeHash() {
+    return SHA256(
+      this.height +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data) +
+        this.nonce
+    ).toString();
+  }
+
+  proofOfWork(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.computeHash();
+    }
+  }
+}
+
+class CryptoBlockchain {
+  constructor() {
+    this.blockchain = [this.startGenesisBlock()];
+    this.difficulty = 4;
+  }
+  startGenesisBlock() {
+    return new CryptoBlock(0, "Initial Block in the Chain", "0");
+  }
+
+  obtainLatestBlock() {
+    return this.blockchain[this.blockchain.length - 1];
+  }
+  addNewBlock(newBlock) {
+    newBlock.previousHash = this.obtainLatestBlock().hash;
+    //newBlock.hash = newBlock.computeHash();
+    newBlock.proofOfWork(this.difficulty);
+    this.blockchain.push(newBlock);
+  }
+
+  checkChainValidity() {
+    for (let i = 1; i < this.blockchain.length; i++) {
+      const currentBlock = this.blockchain[i];
+      const precedingBlock = this.blockchain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.computeHash()) {
+        return false;
+      }
+      if (currentBlock.previousHash !== precedingBlock.hash) return false;
+    }
+    return true;
+  }
+}
+class rupeeCOINTransaction {
+  constructor(sender, recipient, quantity, interestRate) {
+    this.txnId=Date.now().toString(36) + Math.random().toString(36).substr(2);
+    this.sender = sender;
+    this.recipient = recipient;
+    this.quantity = quantity;
+    this.interestRate = interestRate;
+  }
+  //push transaction data in db +++ the hash of txn
+  computeTransactionHash() {
+    return SHA256(
+      this.sender + "sent"+
+        this.recipient + 
+        + this.quantity+
+        "rupeeCOIN at"+ this.interestRate+ "on"+
+        this.timestamp + "txnID" + this.txnId
+    ).toString();
+  }
+}
+let txn = new rupeeCOINTransaction();
+let rupeeCOIN = new CryptoBlockchain();
+
+console.log("rupeeCOIN mining in progress....");
+rupeeCOIN.addNewBlock(
+  new CryptoBlock(1, "01/06/2020", {
+    sender: "Iris Ljesnjanin",
+    recipient: "Cosima Mielke",
+    quantity: 50,
+  })
+);
+
+rupeeCOIN.addNewBlock(
+  new CryptoBlock(2, "01/07/2020", {
+    sender: "Vitaly Friedman",
+    recipient: "Ricardo Gimenes",
+    quantity: 100,
+  })
+);
+
+console.log(JSON.stringify(rupeeCOIN, null, 4));
